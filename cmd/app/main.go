cdf6e986fb1e4001ccac8ccb77f477aa779aa86d
Jose <jose.araujo@itriad.org.br>
2025-04-28 16:44:02 -0300

    [feature/account] feat: create migrations and endpoints

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/josearaujoneto/api-gateway-go/internal/repository"
+	"github.com/josearaujoneto/api-gateway-go/internal/service"
+	"github.com/josearaujoneto/api-gateway-go/internal/web/server"
+	_ "github.com/lib/pq"
+)
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+
+	return defaultValue
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(("Error loading .env file"))
+	}
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		getEnv("DB_HOST", "db"),
+		getEnv("DB_PORT", "5432"),
+		getEnv("DB_USER", "postgres"),
+		getEnv("DB_password", "postgres"),
+		getEnv("DB_NAME", "gateway"),
+		getEnv("DB_SSL_MODE", "disable"),
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err)
+	}
+	defer db.Close()
+
+	accountRepository := repository.NewAccountRepository(db)
+	accountService := service.NewAccountService(accountRepository)
+
+	port := getEnv("HTTP_PORT", "8080")
+	srv := server.NewServer(accountService, port)
+	srv.ConfigureRoutes()
+
+	if err := srv.Start(); err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
 }
